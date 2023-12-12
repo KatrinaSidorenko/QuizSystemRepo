d28fa05e9fbfbe19349e45cbe226efcc93f416a0
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2023-12-12 15:56:08 +0200

    quartz configuration set up

---
+++ QuizSystem/QuizSystem/Jobs/UpdateSharedTestStatusJob.cs
@@ -0,0 +1,12 @@
+﻿using Quartz;
+
+namespace QuizSystem.Jobs;
+
+[DisallowConcurrentExecution]
+public class UpdateSharedTestStatusJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
